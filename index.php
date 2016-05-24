5b904f9bda41bfee7f9ef694a7ba0200d5b0f8b0
mrshake <adam@adamandnicole.com>
2016-05-24 11:04:00 -0500

    removed boilcontrol logic

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
 <link rel="stylesheet" href="css/slider.css">
 <link rel="stylesheet" href="css/bootstrap-formhelpers.min.css">
 <link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" href="css/mbExtruder.css">
 <!-- User Defined CSS Preferences  -->
 <link rel="stylesheet" href="css/userprefs.css">
 
@@ -26,31 +27,34 @@
 <script type="text/javascript" src="framework/segment-display.js"></script>
 <script src="framework/gauge.min.js"></script>
 <script src="framework/bootstrap-formhelpers.js"></script>
+<script src="framework/mbExtruder.js"></script>
+<script src="framework/jquery.mb.flipText.js"></script>
+<script src="framework/jquery.hoverIntent.min.js"></script>
 <script>
 	function printAlarm(id, status)
     {
         if (status == 1) {
             $(id).addClass("btn-danger");
             $(id).removeClass("btn-default");
-            
+
         } else {
             $(id).addClass("btn-default");
             $(id).removeClass("btn-danger");
         }
     }
-    
- 
+
+
 
 	function printVolume(id, volume)
     {
         $(id).html('<small class="text-muted">volume </small>' + volume / 1000.0 + " Gal");
     }
-    
+
     function printTargetVolume(id, target)
     {
         $(id).html('<small class="text-muted">target volume </small>' + target / 1000.0 + " Gal");
     }
-    
+
     function printFlowRate(id, flowrate)
     {
         $(id).html('<small class="text-muted">flow rate </small>' +  flowrate / 1000 + " Gal/min");
@@ -59,12 +63,12 @@
 	{
 		$(id).html("Boil Control: " + data);
 	}
-	
+
     function printProgramThread(id, step, recipe)
     {
         $(id).html("Step: " + step + " Recipe: " + recipe);
     }
-    
+
 /*     function printTimer(id, value, status)
     {
       if(status == 0) {
@@ -74,12 +78,12 @@
       }
       $(id).html("Set Time: " + value/1000 + " seconds <br>Status: " + rStatus);
     } */
-    
+
 //     function printOutputProfiles(id, status)
 //     {
 //         $(id).html("Output Profiles: " + status);
 //     }
-    
+
     function printOutputStatus(id, status)
     {
         $(id).html("Output Status: " + status);
@@ -91,20 +95,20 @@
                                printAlarm(data.alarmStatus);
                                });
     }
-    
+
     function click_buttonAlarmOn()
     {
         brewTrollerExecCommand(BTCMD_SetAlarm, null, {"alarmStatus": "1"}, host, username, password, function(data){
                                printAlarm(data.alarmStatus);
                                });
     }
-    
+
     $(document).ready(function(){
                       });
 </script>
 </head>
 <body>
-  <div class="container">
+  <div class="container" style="padding-left: 30px;">
     <nav class="navbar navbar-default" role="navigation">
         <div class="container-fluid">
             <!-- Brand and toggle get grouped for better mobile display -->
@@ -133,7 +137,7 @@
                             <li><a href="#" onClick="click_buttonAlarmOn();">Set Alarm</a></li>
                         </ul>
                     </li>
-                    
+
                     <li><a href="#" id="button_connect">Connect</a></li>
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">Recipes <b class="caret"></b></a>
@@ -159,7 +163,7 @@
            </div>
            <div class="panel-body">
   			 <p id="programSlot1" class="alert alert-success" style="padding:5px;"><span id="currStatusProg1"></span></p>
-  		   </div>	
+  		   </div>
        	 </div>
        	</div>
        	<div class="col-sm-6">
@@ -172,7 +176,7 @@
            </div>
            <div class="panel-body">
   			 <p id="programSlot2" class="alert alert-success" style="padding:5px;"><span id="currStatusProg2"></span></p>
-  		   </div>	
+  		   </div>
        	  </div>
         </div>
       </div>
@@ -188,7 +192,7 @@
                     <div class="panel-body">
                     	<div id="hltTempSet">
 	                    	<span id="div_hltTemperature">
-	                    		<canvas id="hltGauge" width="200" height="200"></span> 
+	                    		<canvas id="hltGauge" width="200" height="200"></span>
 	                    	<span id="div_hltSetpoint"></span>
                     	</div>
                       <div id="div_hltVolume"></div>
@@ -202,7 +206,7 @@
 														</div>
                         	</div>
                         	<div class="col-md-4">
-                        		<button id="hltTempSetBtn" class="btn btn-default" type="button"">Set Temp</button>
+                        		<button id="hltTempSetBtn" class="btn btn-default" type="button">Set Temp</button>
 												  </div>
                         </div>
                     </div>
@@ -226,7 +230,7 @@
                         	</span>
                         	<canvas class="seg" id="mashDisplay" width="200" height="50">
 													  Your browser is unfortunately not supported.
-													</canvas> 
+													</canvas>
                         	<span id="div_mashSetpoint">
                         	</span>
                         </div>
@@ -241,7 +245,7 @@
 														</div>
                         	</div>
                         	<div class="col-md-4">
-                        		<button id="mashTempSetBtn" class="btn btn-default" type="button"">Set Temp</button>
+                        		<button id="mashTempSetBtn" class="btn btn-default" type="button">Set Temp</button>
 												  </div>
                         </div>
                         <div class="row">
@@ -315,7 +319,7 @@
 						<div class="col-sm-12">
 							<div class="row">
 								<div class="col-sm-12">
-									
+
 								</div>
 							</div>
 							<div class="row">
@@ -563,7 +567,7 @@
             </div>
         </div>
     </div>
-    
+
     <!-- Status Data modal -->
     <div class="modal fade" id="statusData" tabindex="-1" role="dialog" aria-labelledby="statusDataLabel" aria-hidden="true">
         <div class="modal-dialog">
@@ -580,7 +584,7 @@
             </div>
         </div>
     </div>
-    
+
     <!-- Settings modal -->
     <div class="modal fade" id="modal_settings" tabindex="-1" role="dialog" aria-labelledby="settingsLabel" aria-hidden="true">
         <div class="modal-dialog">
@@ -629,7 +633,7 @@
                     <div id="recipeDetails">
 						<table class="table table-condensed">
 						  <tbody>
-						  	
+
 						  </tbody>
 						</table>
 					</div>
@@ -648,7 +652,7 @@
                     <h4 class="modal-title" id="myModalLabel">Load Beer XML</h4>
                 </div>
                 <div class="modal-body" id="loadBeerXMLModalBody">
-                	<input type="file" id="file" name="myFile" />  
+                	<input type="file" id="file" name="myFile" />
                 	<label>
                 	Select a recipe slot to load recipe in to:
                 	<select class="form-control" id="loadProgramNumber">
@@ -671,8 +675,8 @@
 					  <option>17</option>
 					  <option>18</option>
 					  <option>19</option>
-					</select>   
-					</label>          
+					</select>
+					</label>
                 </div>
                 <div class="modal-footer">
                 <button type="button" id="loadBeerXMLButton" class="btn btn-default">Save Recipe to Brewtroller</button>
@@ -696,6 +700,121 @@
         </div>
     </div>
   </div>
+  <div id="extruderLeft" class="{title:'Settings'}">
+  <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingOne">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          Network Setup
+        </a>
+      </h4>
+    </div>
+    <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+      <div class="panel-body">
+      	<form class="form-horizontal" role="form">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Host</label>
+                            <input type="email" class="form-control" id="settingsHost" placeholder="Enter hostname or IP">
+                        </div>
+                        <div class="checkbox">
+                            <label>
+                                <input type="checkbox"> Use authentication
+                            </label>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Username</label>
+                            <input type="email" class="form-control" id="settingsUser" placeholder="Enter email">
+                                </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Password</label>
+                            <input type="password" class="form-control" id="settingsPassword" placeholder="Password">
+                        </div>
+                    </form>
+      </div>
+      <div class="panel-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                    <button id="settingsSaveBtn" type="button" class="btn btn-default" data-dismiss="modal">Save</button>
+                </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingTwo">
+      <h4 class="panel-title">
+        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          Temperature Probe Settings
+        </a>
+      </h4>
+    </div>
+    <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+      <div class="panel-body">
+      	<ul>
+      		<li>HLT: [Address]</li>
+      		<li>Mash: [Address]</li>
+      		<li>Boil: [Address]</li>
+      		<li>Chill: [Address]</li>
+      		<li>Aux 1: [Address]</li>
+      		<li>Aux 2: [Address]</li>
+      		<li>Aux 3: [Address]</li>
+      	</ul>
+      	<select>
+      		<option value="">HLT</option>
+      		<option value="">Mash</option>
+      		<option value="">Boil</option>
+      		<option value="">Chill</option>
+      		<option value="">Aux 1</option>
+      		<option value="">Aux 2</option>
+      		<option value="">Aux 3</option>
+      	</select>
+      	<button>Scan and Set</button>
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingThree">
+      <h4 class="panel-title">
+        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          Valve Profile Settings
+        </a>
+      </h4>
+    </div>
+    <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+      <div class="panel-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingFour">
+      <h4 class="panel-title">
+        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+          Recipe Settings
+        </a>
+      </h4>
+    </div>
+    <div id="collapseFour" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFour">
+      <div class="panel-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingFive">
+      <h4 class="panel-title">
+        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+          Misc Settings
+        </a>
+      </h4>
+    </div>
+    <div id="collapseFive" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFive">
+      <div class="panel-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
 </body>
 <script src="js/brewtroller.js"></script>
 <script src="js/cgiByteCodeMap.js"></script>
